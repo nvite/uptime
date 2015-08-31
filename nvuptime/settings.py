17eb3eea532f39d769e0994abbd89ed678e8c324
Dan Drinkard <dan.drinkard@gmail.com>
2015-08-30 22:34:40 -0400

    serve static from heroku

--- nvuptime/settings.py
+++ nvuptime/settings.py
@@ -18,7 +18,6 @@ import sys
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
@@ -106,7 +105,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 
 
 # Email
