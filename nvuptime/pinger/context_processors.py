fb932a339fec1d7f14dd8dfad5e24d7f53c26320
Dan Drinkard <dan.drinkard@gmail.com>
2015-10-06 15:01:53 -0400

    clean up / fix context processor for outages

--- nvuptime/pinger/context_processors.py
+++ nvuptime/pinger/context_processors.py
@@ -2,12 +2,7 @@ from nvuptime.pinger.models import Outage
 
 
 def outages(request):
-    current_outage = None
-    most_recent_outage = None
-    if Outage.objects.already_down():
-        current_outage = Outage.objects.active()[0]
-        recent_outage = Outage.objects.most_recent()
-        return {
-            'current_outage': current_outage,
-            'recent_outage': recent_outage
-        }
+    return {
+        'current_outage': Outage.objects.active()[0],
+        'recent_outage': Outage.objects.most_recent()
+    }
